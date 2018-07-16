22a488f1163d6c40f7077ed19b6bbbeb833f9eaf
Risingvirtue <risingvirtue@berkeley.edu>
2018-07-15 18:03:13 -0700

    rsi

--- stockML.py
+++ stockML.py
@@ -4,10 +4,41 @@ class StockAPI:
 		self.apikey = apikey
 	def __str__(self):
 		return self.apikey
-	def apicall(self):
-		resp = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=amzn&apikey=]' + self.apikey)
-		print(resp.json())
-		
+	def createText(self, file):
+		return
+	def apicall(self, file):
+		resp = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=amzn&outputsize=full&apikey=' + self.apikey).json()
+		rsi = requests.get('https://www.alphavantage.co/query?function=RSI&symbol=AMZN&interval=daily&time_period=14&outputsize=full&series_type=close&apikey=' + self.apikey).json()
+		resp = resp['Time Series (Daily)']
+		rsi = rsi["Technical Analysis: RSI"]
+		for day in rsi:
+			if (day in resp):
+				resp[day]["RSI"] = rsi[day]["RSI"]
+		print(resp)
+		'''
+		f = open(file, 'w+')
+		f.write(str(resp.json()))
+		f.close()
+		'''
+	def inquiry(self, file):
+		f = open(file, "r")
+		info = f.read()
+		return info
+	def structureFile(self, dict):
+		time = dict['Time Series (Daily)']
+		timeArr = []
+		for day in time:
+			timeArr.append({"day": day, 
+							"open": time[day]["1. open"], 
+							"high": time[day]["2. high"], 
+							"low": time[day]["3. low"], 
+							"close": time[day]["4. close"], 
+							"volume": time[day]["5. volume"] 
+							})
+							
+			
 api = StockAPI('WDY9UDH99K9I5MMI')
-
-api.apicall();
+api.apicall('test')
+##dict = eval(api.inquiry("amzn.txt"))
+##api.structureFile(dict)
+#api.apicall();
